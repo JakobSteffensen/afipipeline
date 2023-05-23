8ecd05bbcedd8b545aeddce046c328f90def219e
ANIMDK <afijst@gmail.com>
2023-05-23 11:06:21 +0200

    Setting up..

--- python/afilm/cmd/afilaunch.py
+++ python/afilm/cmd/afilaunch.py
@@ -5,3 +5,30 @@
 #
 # A Wrapper for launching software inside of a production context.
 # ----------------------------------------------------------------------------------------------------------------------
+
+from PySide2 import QtWidgets
+import sys
+
+
+# Build the UI.
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+
+        self.setWindowTitle("afiLaunch")
+        self.create_menus()
+
+        self.show()
+
+    def create_menus(self):
+        self.file_menu = self.menuBar().addMenu("&File")
+        self.help_menu = self.menuBar().addMenu("&Help")
+
+
+# Open Launcher window.
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    sys.exit(app.exec_())
+
+
