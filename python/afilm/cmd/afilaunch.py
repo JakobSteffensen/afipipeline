692cf09dbe693839168437ac93c3644482c9d3be
ANIMDK <afijst@gmail.com>
2023-05-23 11:38:40 +0200

    Setting up..

--- python/afilm/cmd/afilaunch.py
+++ python/afilm/cmd/afilaunch.py
@@ -6,8 +6,9 @@
 # A Wrapper for launching software inside of a production context.
 # ----------------------------------------------------------------------------------------------------------------------
 
-from PySide2 import QtWidgets
 import sys
+from PySide2 import QtWidgets
+import afilm.core.ui
 
 
 # Build the UI.
@@ -16,19 +17,37 @@ class MainWindow(QtWidgets.QMainWindow):
         QtWidgets.QMainWindow.__init__(self)
 
         self.setWindowTitle("afiLaunch")
+        self.create_actions()
         self.create_menus()
+        self.create_statusbar()
 
-        self.show()
+
+        self._main_widget = QtWidgets.QPushButton('Hello')
+        self.setCentralWidget(self._main_widget)
+
+        afilm.core.ui.center_on_screen(self)
+
+    def create_actions(self):
+        """"""
+        pass
 
     def create_menus(self):
+        """"""
         self.file_menu = self.menuBar().addMenu("&File")
         self.help_menu = self.menuBar().addMenu("&Help")
+
+    def create_statusbar(self):
+        """"""
+        self.statusbar_label = QtWidgets.QLabel("")
+        self.statusBar().showMessage("Ready")
+        self.statusBar().addPermanentWidget(self.statusbar_label)
 
 
 # Open Launcher window.
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
+    window.show()
     sys.exit(app.exec_())
 
 
--- python/afilm/core/ui.py
+++ python/afilm/core/ui.py
@@ -5,3 +5,19 @@
 #
 # UI utility functions.
 # ----------------------------------------------------------------------------------------------------------------------
+
+import webbrowser
+from PySide2 import QtGui
+from PySide2 import QtWidgets
+
+
+def center_on_screen(widget):
+    """"""
+    screen = QtWidgets.QDesktopWidget().screenGeometry()
+    size = widget.geometry()
+    widget.move((screen.width() - size.width()) / 2, (screen.height() - size.height()) / 2)
+
+
+def help(topic=None):
+    """ Open a webbrowser on the current help topic."""
+    webbrowser.open('https://anim.dk/')
